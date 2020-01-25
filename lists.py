f13f0ec871e256b39547a6e055b67958f1785435
Arslaan123Iqbal <sunnybhattimgt3@gmail.com>
2020-01-25 14:18:19 +0500

    First commit

---
+++ lists.py
@@ -0,0 +1,7 @@
+cities=['Faisalabad','Islamabad','Karachi','Lahore','Quetta']
+result=[93.4,95,98.7,87.4,83.02]
+score=[45,70,54,90,42]
+
+print(cities)
+print(result)
+print(score)
---
+++ radius.py
@@ -0,0 +1,9 @@
+
+# To find the Area of circle
+
+r=5
+pi=3.14
+
+A=pi*r*r
+
+print(A)
